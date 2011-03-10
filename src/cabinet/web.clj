31598e8a6056cca778aff6815f43fd94d0b3db22
Leroy Campbell <leroy@artisonian.com>
2011-03-10 15:35:05 -0500

    Added JSON API routes.

--- src/cabinet/web.clj
+++ src/cabinet/web.clj
@@ -1,7 +1,8 @@
 (ns cabinet.web
   (:use compojure.core)
   (:use ring.middleware.json-params)
-  (:require [clj-json.core :as json]))
+  (:require [clj-json.core :as json])
+  (:require [cabinet.elem :as elem]))
 
 (defn json-response [data & [status]]
   {:status (or status 200)
@@ -12,7 +13,16 @@
   (GET "/" []
        (json-response {"hello" "world"}))
   (PUT "/" [name]
-       (json-response {"hello" name})))
+       (json-response {"hello" name}))
+  (context "/elems" []
+           (GET "/" []
+                (json-response (elem/list)))
+           (GET "/:id" [id]
+                (json-response (elem/get id)))
+           (PUT "/:id" [id attrs]
+                (json-response (elem/put id attrs)))
+           (DELETE "/:id" [id]
+                   (json-response (elem/delete id)))))
 
 (def app
   (-> handler
