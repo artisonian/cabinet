9adadd54a20003961deec923776b7bbb81c562fd
Leroy Campbell <leroy@artisonian.com>
2011-03-10 15:47:22 -0500

    Added error handling code.

--- src/cabinet/elem.clj
+++ src/cabinet/elem.clj
@@ -1,4 +1,5 @@
 (ns cabinet.elem
+  (:use clojure.contrib.condition)
   (:refer-clojure :exclude (list get delete)))
 
 (def elems (atom {}))
@@ -7,12 +8,17 @@
   @elems)
 
 (defn get [id]
-  (@elems id))
+  (or (@elems id)
+      (raise :type :not-found
+             :message (format "elem '%s' not found" id))))
 
 (defn put [id attrs]
-  (let [new-attrs (merge (get id) attrs)]
-    (swap! elems assoc id new-attrs)
-    new-attrs))
+  (if (empty? attrs)
+    (raise :type :invalid
+           :message "attrs are empty")
+    (let [new-attrs (merge (@elems id) attrs)]
+     (swap! elems assoc id new-attrs)
+     new-attrs)))
 
 (defn delete [id]
   (let [old-attrs (get id)]
--- src/cabinet/web.clj
+++ src/cabinet/web.clj
@@ -2,7 +2,24 @@
   (:use compojure.core)
   (:use ring.middleware.json-params)
   (:require [clj-json.core :as json])
-  (:require [cabinet.elem :as elem]))
+  (:require [cabinet.elem :as elem])
+  (:import org.codehaus.jackson.JsonParseException)
+  (:import clojure.contrib.condition.Condition))
+
+(def error-codes
+  {:invalid 400
+   :not-found 404})
+
+(defn wrap-error-handling [handler]
+  (fn [req]
+    (try
+      (or (handler req)
+          (json-response {"error" "resource not found"} 404))
+      (catch JsonParseException e
+        (json-response {"error" "malformed json"} 400))
+      (catch Condition e
+        (let [{:keys [type message]} (meta e)]
+          (json-response {"error" message} (error-codes type)))))))
 
 (defn json-response [data & [status]]
   {:status (or status 200)
@@ -26,4 +43,5 @@
 
 (def app
   (-> handler
-      wrap-json-params))
+      wrap-json-params
+      wrap-error-handling))
