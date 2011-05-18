50d6b4b6440c0c74619173e9ba153df5cf2fd734
Leroy Campbell <leroy@artisonian.com>
2011-05-18 12:14:43 -0400

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject cabinet "0.0.1"
+(defproject cabinet "0.0.2"
   :description "REST datastore interface."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [ring/ring-jetty-adapter "0.3.7"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [ring-json-params "0.1.3"]
-                 [compojure "0.6.1"]
-                 [clj-json "0.3.1"]]
-  :dev-dependencies [[lein-ring "0.3.2"]]
+                 [compojure "0.6.3"]
+                 [clj-json "0.3.2"]]
+  :dev-dependencies [[lein-ring "0.4.0"]]
   :ring {:handler cabinet.web/app})
