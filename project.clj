9f5f47b20bde6ec1c032ed29428742a213608763
Leroy Campbell <leroy@artisonian.com>
2011-05-21 18:35:42 -0400

    Added Hassium as a dependency for MongoDB

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [ring-json-params "0.1.3"]
                  [compojure "0.6.3"]
-                 [clj-json "0.3.2"]]
+                 [clj-json "0.3.2"]
+                 [hassium "0.2.1"]]
   :dev-dependencies [[lein-ring "0.4.0"]]
   :ring {:handler cabinet.web/app})
