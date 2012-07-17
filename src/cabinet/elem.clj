47a120df9ad73811bf30c44b71f515a6430f42ab
Pieter Laeremans <pieter@laeremans.org>
2012-07-17 22:26:16 +0200

    Updated cabinet to work with clojure 1.4 and up to date versions of the library

--- src/cabinet/elem.clj
+++ src/cabinet/elem.clj
@@ -1,5 +1,5 @@
 (ns cabinet.elem
-  (:use clojure.contrib.condition)
+  (:use slingshot.slingshot)
   (:refer-clojure :exclude (list get delete)))
 
 (def elems (atom {}))
@@ -9,18 +9,19 @@
 
 (defn get [id]
   (or (@elems id)
-      (raise :type :not-found
+      (throw :type :not-found
              :message (format "elem '%s' not found" id))))
 
 (defn put [id attrs]
   (if (empty? attrs)
-    (raise :type :invalid
+    (throw :type :invalid
            :message "attrs are empty")
     (let [new-attrs (merge (@elems id) attrs)]
       (swap! elems assoc id new-attrs)
       new-attrs)))
 
 (defn delete [id]
-  (let [old-attrs (get id)]
+
+ (let [old-attrs (get id)]
     (swap! elems dissoc id)
     old-attrs))
